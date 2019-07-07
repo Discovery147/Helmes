4287fdb1ac7fbf755473ba4690c44c310672fab4
Valeriy <Sizonenkov96@yandex.ru>
2019-07-07 17:41:17 +0300

    Update algorithm

--- src/main/java/com/helmes/service/AnagramService.java
+++ src/main/java/com/helmes/service/AnagramService.java
@@ -1,29 +1,38 @@
 package com.helmes.service;
 
-import java.util.Collection;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
+import java.util.*;
 
 class AnagramService {
     Collection collectAnagrams(String[] dictionary, String searchAnagramsFor) {
-        final String searchAnagramsFinalFor = searchAnagramsFor.toLowerCase();
-        return Stream.of(dictionary)
-                .parallel()
-                .filter(word -> word.length() == searchAnagramsFor.length())
-                .filter(word -> isAnagram(word.toLowerCase(), new StringBuilder(searchAnagramsFinalFor)))
-                .collect(Collectors.toSet());
-    }
+        char[] originalArray = searchAnagramsFor.toCharArray();
+        Arrays.sort(originalArray);
+        int originalHash = calculateHashCode(originalArray);
 
-    private boolean isAnagram(String wordFromDictionary, StringBuilder searchAnagramsFor) {
-        for (Character sybmol : wordFromDictionary.toCharArray()) {
-            int index = searchAnagramsFor.indexOf(String.valueOf(sybmol));
-            if (index == -1) {
-                return false;
-            } else {
-                searchAnagramsFor.deleteCharAt(index);
+        HashSet<String> set = new HashSet<>();
+        for (String word : dictionary) {
+            if (word.length() == originalArray.length && isAnagram(word, originalArray, originalHash)) {
+                set.add(word);
             }
         }
-        return true;
+        return set;
     }
+
+    private boolean isAnagram(String wordFromDictionary, char[] original, int originalHash) {
+        char[] arrayFromDictionary = wordFromDictionary.toCharArray();
+        int hashFromDictionary = calculateHashCode(arrayFromDictionary);
+        if (originalHash == hashFromDictionary) {
+            Arrays.sort(arrayFromDictionary);
+            return Arrays.equals(arrayFromDictionary, original);
+        }
+        return false;
+    }
+
+    private int calculateHashCode(char[] arrayFromDictionary) {
+        int hashFromDictionary = 0;
+        for (char c : arrayFromDictionary) {
+            hashFromDictionary += (c % 32) * 32;
+        }
+        return hashFromDictionary;
+    }
+
 }
